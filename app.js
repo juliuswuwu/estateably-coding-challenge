d5c787c6ddfb6c8c0c9ef77c217cecfef17982a4
Harrison Higgins <harryrhiggins@gmail.com>
2021-02-13 19:09:42 -0700

    Added test route for transactions

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const transactions = require('./routes/api/transactions');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db, { useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-app.get("/", (req, res) => res.send("Hello World"));
+app.get('/', (req, res) => res.send('Hello World'));
+app.use('/api/transactions', transactions);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
