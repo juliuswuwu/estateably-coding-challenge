f66f0596efaa82209ef66bb525ad28a6dbb3c7ca
Harrison Higgins <harryrhiggins@gmail.com>
2021-02-14 20:43:22 -0700

    Basic setup of redux-form

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,18 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import TransactionForm from './TransactionForm';
 
 function App() {
+  const submit = values => {
+    console.log(values);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Estateably Code Challenge</h1>
       </header>
+      <TransactionForm onSubmit={submit} />
     </div>
   );
 }
