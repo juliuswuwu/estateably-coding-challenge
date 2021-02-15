4f75c9fae7698dccb2c11b677d502649571afa97
Harrison Higgins <harryrhiggins@gmail.com>
2021-02-14 23:51:01 -0700

    Created TransactionsTable component and styling, added main styles

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,44 +1,35 @@
 // import logo from './logo.svg';
-// import './App.css';
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import './App.css';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchTransactions, createTransaction } from '../actions/transactions';
 import FilterTransactionsForm from './FilterTransactionsForm';
+import TransactionsTable from './TransactionsTable';
 import TransactionForm from './TransactionForm';
 
 function App() {
-  const transactions = useSelector(state => state.transactions);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchTransactions());
-  }, []);
-
   const submit = values => {
-    console.log(values);
     dispatch(createTransaction(values));
   }
 
   const filter = values => {
-    console.log(values);
     dispatch(fetchTransactions(values));
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Estateably Code Challenge</h1>
+        <img className="logo" src="https://uploads-ssl.webflow.com/5f8cd301c7688b13baf71568/5f8f7c0a813b9ec68d9994ad_Group%20129.svg" />
+        <h1>Code Challenge</h1>
+        <h2>Harry Higgins</h2>
       </header>
-      <FilterTransactionsForm onSubmit={filter} />
-      <div>
-        <ul>
-          {Object.keys(transactions).map(id => (
-            <li key="id">{`${transactions[id].description}, ${transactions[id].category}, ${transactions[id].value}`}</li>
-          ))}
-        </ul>
-      </div>
-      <TransactionForm onSubmit={submit} />
+      <main>
+        <FilterTransactionsForm onSubmit={filter} />
+        <TransactionsTable />
+        <TransactionForm onSubmit={submit} />
+      </main>
     </div>
   );
 }
---
+++ frontend/src/components/TransactionsTable.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { fetchTransactions } from '../actions/transactions';
+import './TransactionsTable.css';
+
+const TransactionsTable = props => {
+  const transactions = useSelector(state => state.transactions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+      {Object.keys(transactions).map(id => (
+        <tr key={id}>
+          <td>{transactions[id].createdAt}</td>
+          <td>{transactions[id].description}</td>
+          <td>{transactions[id].category}</td>
+          <td>{transactions[id].value}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionsTable;
